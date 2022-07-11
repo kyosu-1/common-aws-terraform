d6a6a504280ed483f0cc56c8ffeb0986e505f5cf
kyosu-1 <sho013039@gmail.com>
2022-07-11 23:04:31 +0900

    apply github oidc

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ terraform {
     aws = "~>4.0"
   }
 }
+
+data "tls_certificate" "github_actions_oidc_provider" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+# oidc for github actions
+resource "aws_iam_openid_connect_provider" "github_actions_oidc" {
+  url = "https://token.actions.githubusercontent.com"
+  thumbprint_list = [
+    data.tls_certificate.github_actions_oidc_provider.certificates[0].sha1_fingerprint
+  ]
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
